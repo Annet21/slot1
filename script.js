ca79b8407b2230dbe97fb4a6d072eb57dfc3bb85
Annet21 <lebedenkoanna85@gmail.com>
2024-12-05 18:35:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let userScore = 0;
+const userName = prompt("Введіть Ваше ім'я:");
+document.getElementById("name").textContent = userName;
+
+const slotImages = [
+    'apple.png', 
+    'lemon.png',
+    'grape.png',
+    'cherry.png',
+    'orange.png'
+];
+
+function generateNumbers() {
+    const slots = [];
+    const spinCount = 10; 
+    const spinDuration = 100; 
+
+    for (let spin = 0; spin < spinCount; spin++) {
+        setTimeout(() => {
+            for (let i = 0; i < 9; i++) {
+                let randomIndex = Math.floor(Math.random() * slotImages.length);
+                slots[i] = slotImages[randomIndex];
+                document.getElementById(`slot${i + 1}`).style.backgroundImage = `url(${slots[i]})`;
+            }
+
+            if (spin === spinCount - 1) {
+                setTimeout(() => {
+                    checkWin(slots);
+                }, 200); 
+            }
+        }, spin * spinDuration);
+    }
+}
+
+function checkWin(slots) {
+    const centerRowImages = [slots[3], slots[4], slots[5]]; 
+    const allEqual = centerRowImages.every(val => val === centerRowImages[0]);
+
+    if (allEqual) {
+        alert(`${userName}, ви виграли!`);
+    } else {
+        alert("Спробуйте ще раз.");
+    }
+}
+
+document.getElementById("generate").addEventListener("click", generateNumbers);
